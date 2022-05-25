2bb1d2f7401611e3d91a151f8bf098b99f1781d2
pmagbor <pma@trackunit.com>
2022-05-25 17:33:41 -0400

    Added entity and repository for 'RESERVATION' table in schema

---
+++ src/main/java/com/paulmagbor/lil/learningspring/data/Reservation.java
@@ -0,0 +1,69 @@
+package com.paulmagbor.lil.learningspring.data;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "RESERVATION")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "RESERVATION_ID")
+    private long reservationId;
+
+    @Column(name = "ROOM_ID")
+    private long roomId;
+
+    @Column(name = "GUEST_ID")
+    private long guestId;
+
+    @Column(name = "DATE")
+    private Date date;
+
+    // getters and setters
+    public long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public long getGuestId() {
+        return guestId;
+    }
+
+    public void setGuestId(long guestId) {
+        this.guestId = guestId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation [date=" + date + ", guestId=" + guestId + ", reservationId=" + reservationId + ", roomId="
+                + roomId + "]";
+    }
+
+    
+}
---
+++ src/main/java/com/paulmagbor/lil/learningspring/data/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.paulmagbor.lil.learningspring.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    
+}
